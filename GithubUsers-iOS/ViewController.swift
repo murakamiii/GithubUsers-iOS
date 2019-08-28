e13271c5babdad476fa4c3330a06a16d60ffe271
murakami <9khc727ru7@gmail.com>
2019-08-28 22:24:32 +0900

    Initial Commit

---
+++ GithubUsers-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubUsers-iOS
+//
+//  Created by murakami Taichi on 2019/08/28.
+//  Copyright © 2019 murakammm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
